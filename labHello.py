4e529cc670fda632fb077e440377fd09f7ee752a
Liza Tolkachoyva <artlizok2003h@gmail.com>
2023-02-09 11:33:45 +0300

    labs

---
+++ labHello.py
@@ -0,0 +1,18 @@
+
+print('Hello' * 5)
+
+name = "Миша"
+print("Привет, " + name + "!")
+print(f"Привет, {name} !")
+print("Привет, " , name , "!")
+
+print(1,2,3,4)
+print(1,2,3,4, sep=',')
+print(1,2,3,4, sep='\n')
+
+def AAA():
+    a = 1
+    b = 2
+    print(a + b)
+
+AAA()
